325c54e95ae0cb157ea6bed4fc301b75ec204cba
Roger <morenoroger001@gmail.com>
2023-04-23 03:10:54 -0400

    feat: sync slider on second page to move home slider

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__);
 socketio = SocketIO(app)
@@ -17,9 +17,19 @@ def second():
 
 # socket event functions
 @socketio.on('server_connect')
-def handle_conection(dataIn):
-    print(dataIn['data'])
+def handle_conection(data):
+    print(data['data'])
 
+@socketio.on('slider_moved')
+def handle_slider_move(data):
+    print('slider moved with value = ' + data['value'] + '\n')
+    emit('move_slider', {'value': data['value']}, room='stream_room')
+
+# room event functions
+@socketio.on('join_room')
+def on_join_room(data):
+    room = data['room']
+    join_room(room)
 
 
 if __name__ == '__main__':
