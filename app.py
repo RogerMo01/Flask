638c9e57ed9532c65d96685f46696cde4fc99b2f
Roger <morenoroger001@gmail.com>
2023-04-23 01:17:30 -0400

    feat: Initialize socket-io

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__);
+socketio = SocketIO(app)
 
 
-
+# view functions
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,6 +15,12 @@ def second():
     return render_template('second.html')
 
 
+# socket event functions
+@socketio.on('server_connect')
+def handle_conection(dataIn):
+    print(dataIn['data'])
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
